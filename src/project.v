0b746ee8147f7a69e28f11ec1b9768aef3f95d3f
Prajwal M <112241764+PrajwalM28@users.noreply.github.com>
2025-09-06 13:01:22 +0530

    Update project.v
    initial design commit of a 2x1 mux

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module mux_2x1_bmsce(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,22 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    assign s = ui_in[0];
+    assign i0 = ui_in[1];
+    assign i1 = ui_in[2];
 
+    assign out = s? i1 : i0;
+
+
+    assign uo_out[0] = out;
+    assign uo_out[7:1] = 7'b0;
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+ // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0,u0_in[7:3]};
 
 endmodule
